z3ntu/GangstarMiami_Unity

commit fddc5e58355af76a017d9335d10759ef1cd6a72e

Fix deprecated method

Assets/Scripts/GameController.cs
@@ -24,7 +24,7 @@ public class GameController : MonoBehaviour {
 		WWW www = new WWW ("file://" + data_location + "/" + soundname);
 		AudioClip myAudioClip = www.GetAudioClip();
 
-		while (!myAudioClip.isReadyToPlay)
+		while (myAudioClip.loadState != AudioDataLoadState.Failed)
 			yield return www;
 
 		AudioClip clip = www.GetAudioClip(false);

commit 7fa4a7807c4cc385edcfa3f149c0e8ab97792e7a

Fix audio not loading (caused by fddc5e5)

Assets/Scripts/GameController.cs
@@ -27,7 +27,7 @@ public class GameController : MonoBehaviour
         WWW www = new WWW("file://" + data_location + "/" + soundname);
         AudioClip myAudioClip = www.GetAudioClip();
 
-        while (myAudioClip.loadState != AudioDataLoadState.Failed)
+        while (myAudioClip.loadState != AudioDataLoadState.Loaded)
             yield return www;
 
         AudioClip clip = www.GetAudioClip(false);